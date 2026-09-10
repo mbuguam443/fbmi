2f3604ef78bd5d38742096119b198f2e6a1870c3
Mathew Mbugua <mbuguam443@gmail.com>
2026-09-10 11:13:32 +0300

    Fix sidebar: events/ministries point to staff views

--- pull.py
+++ pull.py
@@ -1,9 +1,5 @@
-import os, subprocess
+import os
 os.chdir('/home/wlsihszp/fbmi')
-
-result = subprocess.run(['git', 'log', '--oneline', '-3'], capture_output=True, text=True)
-print("Latest commits on server:")
-print(result.stdout)
-print("---")
-result2 = subprocess.run(['git', 'status'], capture_output=True, text=True)
-print(result2.stdout)
+os.system("git fetch origin")
+os.system("git reset --hard origin/main")
+print("Updated to latest from GitHub.")
